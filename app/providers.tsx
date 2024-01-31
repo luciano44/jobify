947788d79c5e73d431f6ea32b82f19ce2c4ec177
Luciano Ribeiro <luciano.ar6@gmail.com>
2024-01-31 05:04:23 -0300

    add theme toggle navbar feature

---
+++ app/providers.tsx
@@ -0,0 +1,22 @@
+"use client"
+import ThemeProvider from "@/components/theme-provider"
+
+type Props = {
+  children: React.ReactNode
+}
+
+const Providers = ({ children }: Props) => {
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  )
+}
+export default Providers
