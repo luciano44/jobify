1862c0b79b26effc3d729a7272e8efaba848614c
Luciano Ribeiro <luciano.ar6@gmail.com>
2024-02-08 18:15:28 -0300

    setup react-query

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,25 @@
 "use client"
 import ThemeProvider from "@/components/theme-provider"
+import { useState } from "react"
 import { Toaster } from "@/components/ui/sonner"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 type Props = {
   children: React.ReactNode
 }
 
 const Providers = ({ children }: Props) => {
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 1000 * 60 * 5,
+        },
+      },
+    })
+  })
+
   return (
     <>
       <ThemeProvider
@@ -15,8 +28,11 @@ const Providers = ({ children }: Props) => {
         enableSystem
         disableTransitionOnChange
       >
-        {children}
         <Toaster />
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   )
